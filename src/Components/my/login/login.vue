158f2c8f202ea89e1d0bde84e72ef12ac8967f7b
Joey <13169840505@163.com>
2018-05-27 16:03:07 +0800

    完成登录注册界面

---
+++ src/Components/my/login/login.vue
@@ -0,0 +1,62 @@
+<template>
+<div>
+  <ul class="loho_login-mode">
+    <li>
+      <a href="javascript:">
+        手机号码快速登录
+      </a>
+    </li>
+    <li class="loho_active">
+      <a class="loho_active" href="javascript:">
+        帐号密码登录
+      </a>
+    </li>
+  </ul>
+  <div>
+    <account-login></account-login>
+  </div>
+</div>
+  
+</template>
+
+<script>
+import AccountLogin from '../account-login/account-login'
+export default {
+  components:{
+    'account-login':AccountLogin
+  }
+}
+</script>
+
+<style lang="scss">
+.loho_login-mode{
+  display: flex;
+  background: white;
+  li{
+    flex: 1;
+    color: rgb(51, 51, 51);
+    position: relative;
+    a{
+      text-align: center;
+      display: block;
+      line-height: 1.4;
+      padding: 10px;
+      
+    }
+    a.loho_active{
+      color: #e8343b;
+    }
+    &.loho_active::before{
+      content: "";
+      display: block;
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      height: 1.5px;
+      width: 100%;
+      background: #e8343b;
+    }
+   
+  }
+}
+</style>
---
+++ src/Components/my/my-header/my-header.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="loho_header">
+    <a class="icon-back-line" href="javascript:"></a>
+    <h1>登录</h1>
+    <div class="loho_header-right">
+      <i class="icon-qicheqianlian"></i>
+      <i class="icon-list"></i>
+    </div>
+  </div>
+</template>
+
+<style lang='scss'>
+.loho_header{
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 50px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 10px;
+  background: white;
+  a::before {
+    color: rgb(51, 51, 51);
+    font-size: 1.5em;
+  }
+  h1{
+    font-style: normal;
+    font-weight: normal;
+    padding-left: 0.3906rem;
+    font-size: 14px;
+    line-height: 1.4;
+  }
+  .loho_header-right{
+    display: flex;
+    justify-content: space-between;
+    align-items: center; 
+    i:nth-child(1)::before{
+      font-size: 1.5em;
+    }
+    i{
+      padding: 5px;
+    }
+  }
+}
+</style>
