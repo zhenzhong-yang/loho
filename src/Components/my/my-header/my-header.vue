aaf92ff7616015f1bea0b86747baf6e463e03474
les <13169840505@163.com>
2018-05-29 14:38:20 +0800

    详情页完成数据截取

--- src/Components/my/my-header/my-header.vue
+++ src/Components/my/my-header/my-header.vue
@@ -3,13 +3,29 @@
     <a class="icon-back-line" href="javascript:"></a>
     <h1>登录</h1>
     <div class="loho_header-right">
-      <i class="icon-qicheqianlian"></i>
-      <i class="icon-list"></i>
+      <router-link to="/shoppingcar" class="icon-qicheqianlian"></router-link>
+      <i  @click="()=>{showmenu = !showmenu}" class="icon-list"></i>
+      <ul v-if="showmenu" class="loho_show-menu">
+      <li><router-link to="/" > <i class="icon-people"></i> <span>会员中心</span> </router-link></li>
+      <li><router-link to="/" > <i class="icon-shouye"></i> <span>首页</span> </router-link></li>
+      </ul>
     </div>
   </div>
 </template>
 
+<script>
+export default {
+  data(){
+    return {
+      showmenu:false
+    }
+  }
+}
+</script>
+
+
 <style lang='scss'>
+
 .loho_header{
   position: fixed;
   top: 0;
@@ -21,6 +37,7 @@
   align-items: center;
   padding: 0 10px;
   background: white;
+  z-index: 100;
   a::before {
     color: rgb(51, 51, 51);
     font-size: 1.5em;
@@ -36,11 +53,33 @@
     display: flex;
     justify-content: space-between;
     align-items: center; 
+    position: relative;
     i:nth-child(1)::before{
       font-size: 1.5em;
     }
     i{
       padding: 5px;
+    }
+    .loho_show-menu{
+      position: absolute;
+      background: #fff;
+      border: 1px solid #eee;
+      top: 35px;
+      right: 0px;
+      width: 136px;
+      z-index: 50;
+      // height: 6px;
+      a{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 3px 10px;
+        span{
+          flex:1;
+          text-align: center;
+          font-size: 14px;
+        }
+      }
     }
   }
 }
--- src/router/router.js
+++ src/router/router.js
@@ -1,5 +1,5 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 
 Vue.use(VueRouter);
 
@@ -7,13 +7,18 @@ import HomeComponent from "../Components/home/home.vue"
 import ClassifyComponent from "../Components/classify/classify.vue"
 import ExperienceComponent from "../Components/experience/experience.vue"
 import MyComponent from "../Components/my/my.vue"
-
+import RegisterComponent from "../Components/my/my-register/my-register"
+import DetailPageComponent from "../Components/detail-page/detail-page"
+import ShopingcarComponent from "../Components/shopping-car/shopping-car"
 const router = new VueRouter({
     routes: [
         {path: '/', component: HomeComponent, name: 'home'},
         {path: "/classify", component: ClassifyComponent, name: "classify"},
         {path: "/experience", component: ExperienceComponent, name: "experience"},
-        {path: "/my", component: MyComponent ,name: "my"}
+        {path: "/my", component: MyComponent ,name: "my"},
+        {path: "/register", component: RegisterComponent ,name: "register"},
+        {path: "/detailpage", component: DetailPageComponent ,name: "detailpage"},
+        {path: "/shoppingcar", component: ShopingcarComponent ,name: "shoppingcar"},
     ]
 })
 
